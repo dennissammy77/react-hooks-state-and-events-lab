8b06fa43d17d222b17cec89f5fea922a6ef0c936
dennissammy77 <dennissammy77@gmail.com>
2025-04-13 01:05:11 +0300

    hooks and events

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory,setselectedCategory]=useState("")
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e)=>setselectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +14,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {items.filter((item)=>item.category.toLowerCase().includes(selectedCategory.toLowerCase())).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
